2920595bd2721df7c1b2b60f2d626f9b937b149f
Qalib Abbas <43039463+A1webtechies@users.noreply.github.com>
2020-02-09 00:09:42 +0500

    undates

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,4 @@ module.exports = {
   dbURL: "mongodb://localhost:27017/devconnect",
   secretOrKey: "secret"
 };
-// "mongodb://qalibabbas:PSKHNMC(4)@ds011399.mlab.com:11399/churahi_database",
-// mongodb://localhost:27017/devconnect
+
